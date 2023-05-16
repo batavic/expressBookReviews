55803d6a182bdb2c4ee10e9b31fb4a62774cd71a
Victoria Bata <85346019+batavic@users.noreply.github.com>
2023-05-16 20:22:57 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,6 +11,26 @@ public_users.get('/', function (req, res) {
     const formattedBookList = JSON.stringify(bookList, null, 2);
     return res.status(200).send(formattedBookList);
 });
+
+//Using promises
+/*
+public_users.get('/', function (req, res) {
+  new Promise(function(resolve, reject) {
+    const bookList = Object.values(books);
+    const formattedBookList = JSON.stringify(bookList, null, 2);
+    resolve(formattedBookList);
+  })
+  .then(function(formattedBookList) {
+    return res.status(200).send(formattedBookList);
+  })
+  .catch(function(error) {
+    return res.status(500).json({ message: 'Failed to fetch book list' });
+  });
+});
+*/ 
+
+
+
   
 
 // Get book details based on ISBN
@@ -24,7 +44,33 @@ public_users.get('/isbn/:isbn', function (req, res) {
       return res.status(404).json({ message: "Book not found" });
     }
 });
-  
+
+//Using promises
+/* 
+public_users.get('/isbn/:isbn', function (req, res) {
+  const { isbn } = req.params;
+
+  new Promise(function(resolve, reject) {
+    const book = books[isbn];
+    if (book) {
+      resolve({ book });
+    } else {
+      reject({ message: "Book not found" });
+    }
+  })
+  .then(function(data) {
+    return res.status(200).json(data);
+  })
+  .catch(function(error) {
+    return res.status(404).json(error);
+  });
+});
+*/
+
+
+
+
+
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
@@ -37,7 +83,30 @@ public_users.get('/author/:author', function (req, res) {
       return res.status(404).json({ message: "No books found by the author" });
     }
 });
+
+//Using promises
+/* public_users.get('/author/:author', function (req, res) {
+  const { author } = req.params;
+
+  new Promise(function(resolve, reject) {
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+    if (booksByAuthor.length > 0) {
+      resolve({ books: booksByAuthor });
+    } else {
+      reject({ message: "No books found by the author" });
+    }
+  })
+  .then(function(data) {
+    return res.status(200).json(data);
+  })
+  .catch(function(error) {
+    return res.status(404).json(error);
+  });
+});
+*/
   
+
+
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
@@ -49,8 +118,31 @@ public_users.get('/title/:title', function (req, res) {
     } else {
       return res.status(404).json({ message: "No books found with the given title" });
     }
+});
+
+//Using promises
+/* public_users.get('/title/:title', function (req, res) {
+  const { title } = req.params;
+
+  new Promise(function(resolve, reject) {
+    const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+    if (booksByTitle.length > 0) {
+      resolve({ books: booksByTitle });
+    } else {
+      reject({ message: "No books found with the given title" });
+    }
+  })
+  .then(function(data) {
+    return res.status(200).json(data);
+  })
+  .catch(function(error) {
+    return res.status(404).json(error);
   });
+});
+*/
   
+
+
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
@@ -65,6 +157,9 @@ public_users.get('/review/:isbn', function (req, res) {
       return res.status(404).json({ message: "Book not found" });
     }
 });
+
+
+
 
 public_users.post("/register", (req, res) => {
     const { username, password } = req.body;
